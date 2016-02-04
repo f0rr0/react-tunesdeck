b488869789c24b5caab004384225c12484c5324d
Sid <mail@isiddharth.com>
2016-02-04 08:15:29 -0800

    Initial commit

--- node_modules/re-base/examples/github-notetaker/app/App.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import Router from 'react-router';
-import routes from './config/routes';
-
-Router.run(routes, (Root, state) => {
-  React.render(<Root {...state} />, document.getElementById('app'));
-});
