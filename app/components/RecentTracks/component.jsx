98e66f145f22d43a186d3f3f3475f3f012d53e86
Sid <mail@isiddharth.com>
2016-02-04 08:48:22 -0800

    Added Example

--- app/components/RecentTracks/component.jsx
+++ app/components/RecentTracks/component.jsx
@@ -5,14 +5,20 @@ import styles from './style.css';
 
 const Tracks = Rebase.createClass('https://quantifiedself.firebaseio.com');
 
-
 @Radium
 export default class Header extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       tracks: [],
+      today: ''
     };
+    Rebase.listenTo('lastDate', {
+      context: this,
+      then: (date) => {
+        this.setState({ today: date });
+      }
+    });
   }
 
   render() {
@@ -24,21 +30,21 @@ export default class Header extends React.Component {
       )
     });
     return (
-      <ol style={styles.base}>
-        {tracks}
-      </ol>
+      <div style={styles.base}>
+        <h6>{this.state.today}</h6>
+        <ol>
+          {tracks}
+        </ol>
+      </div>
     );
   }
 
-  componentWillMount() {
-    Tracks.bindToState('2016/02/04/tracks', {
+  componentDidMount() {
+    Tracks.bindToState(this.state.today, {
       context: this,
       state: 'tracks',
       asArray: true
     });
-  }
-
-  componentDidMount() {
     console.log(styles);
   }
 }
