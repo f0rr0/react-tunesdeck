634a55b5beb4309ae11b297ee31c8f5f858b5638
Sid <mail@isiddharth.com>
2016-02-24 23:30:12 -0800

    changed width

--- app/main.jsx
+++ app/main.jsx
@@ -12,7 +12,7 @@ class App extends React.Component {
       <div>
         <article
           className='base'
-          style={{ width: 432, backgroundColor: '#4CAB3D', margin: 'auto' }}
+          style={{ width: 452, backgroundColor: '#4CAB3D', margin: 'auto' }}
         >
           Tracks are being scrobbled from Spotify, YouTube, SoundCloud and pretty much every other major music streaming service. A Google Apps script polls Last.fm every minute and logs the tracks to a Firebase database. This React component watches that database and renders the five most recent tracks.<br /><br/><a href='https://github.com/sidjain26/react-tunesdeck' style={{ color: '#FFFAFA' }}>View on Github</a>
         </article>
